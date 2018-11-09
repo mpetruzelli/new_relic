7624b6346375d356132067288fc9c56af72f4ad8
drz087 <michael.petruzelli@capitalone.com>
2018-11-09 10:56:36 -0500

    latest

--- env/bin/activate.csh
+++ env/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/mike/github/new_relic/solution/env"
+setenv VIRTUAL_ENV "/Users/drz087/github/new_relic/env"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
--- src/phrases/test_top_ten.py
+++ src/phrases/test_top_ten.py
@@ -1,7 +1,34 @@
+import unittest
 import subprocess
+import os
+from phrases import top_ten
 
-test = subprocess.check_output("phrases pg2009.txt", shell=True)
-test2 = subprocess.check_output("phrases pg2009.txt", shell=True)
+class TestTopTen(unittest.TestCase):
 
-if test2 == test:
-    print("{}".format("TRUE"))
+    def setUp(self):
+        self.input_list = ["a b c", "A B C", "a B c", "$a %B !C"]
+
+    def test_top_ten_files(self):
+        self.test_results = list()
+        for index,item in enumerate(self.input_list):
+            with open(str(index) + '.txt', 'w') as text_file:
+                text_file.write(item)  
+            self.test_results.append(subprocess.check_output("phrases %s.txt" % index , shell=True))
+            if len(self.test_results) > 2:
+                self.assertEqual(self.test_results[index], self.test_results[1])
+            os.remove(str(index) + '.txt')
+            
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
+# test_file = "pg2009.txt"
+# test_args = "{0} {1}".format("phrases",test_file)
+
+# test = subprocess.check_output(test_args, shell=True)
+# test2 = subprocess.check_output(test_args, shell=True)
+
+
+# if test2 == test:
+#     print("{}".format("TRUE"))
